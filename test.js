924b73da996ecae344c75c61822017df444580af
Lewis Donovan <dono023@GBMC02YX50CLVDQ.(none)>
2021-08-17 23:14:57 -0500

    Add query string object to test file

--- test.js
+++ test.js
@@ -4,6 +4,11 @@ const gns = require('./index.js');
   try {
     const articles = await gns({
       searchTerm: "tech news",
+      queryVars: {
+        hl:"en-US",
+        gl:"US",
+        ceid:"US:en"
+      },
       prettyURLs: true,
       timeframe: "5d",
       puppeteerArgs: [
